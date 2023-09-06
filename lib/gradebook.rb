875bd0c5bfcd1875bc8ed5e939644178e6c4f75f
Anthea Yur <acyur6@gmail.com>
2023-09-06 12:14:22 -0500

    Finished Iteration IV students_in_range

--- lib/gradebook.rb
+++ lib/gradebook.rb
@@ -34,8 +34,26 @@ class Gradebook
     end
   end
 
+  def students_in_range(min, max)
+    students = []
+    @courses.each do |course|
+      course.students.each do |student|
+        students << student
+      end
+    end
+    students.find_all do |student|
+      student.grade >= min && student.grade <= max    
+    end
+  end
+
+  # def all_grades
+  #   all_grades = {}
+  #   @courses.each do |course|
+  #     all_grades.merge!({course=>course.students.grades})
+  #   end
+  # end
 
 end
 
-# binding.pry
+binding.pry
 # list_all_students => {"Physics" => [Student1, Student2], "Math" => [Student3, Student2]}
